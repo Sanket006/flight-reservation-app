1c50f7c801948c86fa18373c539a097dd998e933
Sanket <sanketchopade6@gmail.com>
2026-02-19 18:49:53 +0530

    frontend changes added

--- frontend/frontend-pipeline.groovy
+++ frontend/frontend-pipeline.groovy
@@ -19,7 +19,7 @@ pipeline{
             steps{
                 sh '''
                     cd frontend
-                    aws s3 sync dist/ s3://cblkdfsfdsc-front12end-project-bux/
+                    aws s3 sync dist/ s3://flight-reservation-s3-frontend-bucket/
                 '''    
             }
         }
